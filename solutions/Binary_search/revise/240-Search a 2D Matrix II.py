999e6b05c98136561cfd8b65c5eb3dcf6f211bd4
Rajeshwari T. Golande <127578319+rajeshwari-golande@users.noreply.github.com>
2026-09-01 10:35:22 +0530

    Solve: 240. Search a 2D Matrix II (Medium)

---
+++ solutions/Binary_search/revise/240-Search a 2D Matrix II.py
@@ -0,0 +1,24 @@
+// Pattern: nice algo to understand the workflow
+// Difficulty: Medium
+// Problem: 240. Search a 2D Matrix II
+// Link: https://leetcode.com/problems/search-a-2d-matrix-ii
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m=len(matrix)
+        n=len(matrix[0])
+        low=0
+        high=n-1
+        while(low<m and high<n and high>=0):
+            if matrix[low][high] == target:
+                return True
+            elif matrix[low][high]<target: #move to bigger elements
+                low=low+1
+            else:
+                high=high-1
+        return False
+
+
+
+
+        
