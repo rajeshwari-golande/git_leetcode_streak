5c7e6355748788cb2a4e9753bcb6e6685ed544d8
Rajeshwari T. Golande <127578319+rajeshwari-golande@users.noreply.github.com>
2026-09-15 11:46:09 +0530

    Solve: 1046. Last Stone Weight (Easy)

---
+++ solutions/heap/revise/1046-Last Stone Weight.py
@@ -0,0 +1,25 @@
+// Pattern: easy-->greedy pattern
+// Difficulty: Easy
+// Problem: 1046. Last Stone Weight
+// Link: https://leetcode.com/problems/last-stone-weight
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        # we need to find the 2 max elements in the present array for evvery instant --> max heap
+        heap=[]
+        for stone in stones:
+            heapq.heappush(heap,-stone)
+        # n=len(stones)
+        while(1):
+            if not heap:
+                return 0
+            if len(heap)==1:
+                return -heapq.heappop(heap)
+            first_largest= -heapq.heappop(heap)
+            second_largest=-heapq.heappop(heap)
+            if first_largest!=second_largest:
+                heapq.heappush(heap,-(first_largest-second_largest))
+
+
+
+        
